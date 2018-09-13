d4c56ba6de3770a703317f76188679e5adb0a518
jacopoRufini <jacoporufini97@gmail.com>
2018-09-13 18:03:41 +0200

    temporary!

--- src/app/editor/components/sidebar/image/panels/url.panel.ts
+++ src/app/editor/components/sidebar/image/panels/url.panel.ts
@@ -1,4 +1,5 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'template-url-panel',
@@ -26,7 +27,18 @@ export class UrlPanel {
 
   @Output() selected: EventEmitter<string> = new EventEmitter<string>();
 
+  public constructor(private http: HttpClient) {}
+
   private confirm(): void {
+    let base64: string;
+    this.http.get(this.src, { responseType: 'blob' }).subscribe((blob) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(blob);
+      reader.onloadend = function() {
+        base64 = reader.result;
+        console.log(base64);
+      };
+    });
     this.selected.emit(this.src);
   }
 }
